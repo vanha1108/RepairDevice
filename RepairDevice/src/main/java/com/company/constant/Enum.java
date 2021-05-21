ff743b2f1f18f1265459446e7cdf07dc895855e2
vanha1108 <nguyenvanha11899@gmail.com>
2021-05-22 01:11:54 +0700

    Enum

---
+++ RepairDevice/src/main/java/com/company/constant/Enum.java
@@ -0,0 +1,26 @@
+package com.company.constant;
+
+/**
+ * Nguyễn Văn Hà
+ * 3:39 PM 4/28/2021
+ */
+public class Enum {
+
+    public static final String ADMIN = "ADMIN";
+
+    // Khi nhân viên tạo yêu cầu xong và đang chờ trưởng phòng duyệt
+    public static final String WAIT_MANAGER = "WAIT_MANAGER";
+
+    // Sau khi trưởng phòng duyệt xong sẽ chuyển lên cho giám đốc duyệt
+    public static final String WAIT_DIRECTOR = "WAIT_DIRECTOR";
+
+    // Sau khi giám đốc duyệt xong chuyển qua phòng TCHC chờ xử lý
+    public static final String WAIT_TCHC = "WAIT_TCHC";
+
+    // Sau khi TCHC xem xét sẽ chuyển đến các bộ phận liên quan để tiến hành thực hiện yêu cầu sửa chữa
+    public static final String FIXING = "FIXING";
+
+    // Sau khi sửa chữa xong sẽ trả về lại phòng TCHC để lưu trữ
+    public static final String FINISHED = "FINISHED";
+
+}
