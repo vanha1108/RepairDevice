7644553f3a3734d3610a396512c8cac83d23475f
devroot <maituongwork@gmail.com>
2021-07-18 17:17:10 +0700

    add socket ag

--- RepairDevice/src/main/java/com/company/configs/WebSocketConfig.java
+++ RepairDevice/src/main/java/com/company/configs/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/sock-data").setAllowedOrigins().withSockJS();
+        registry.addEndpoint("/sock-data").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
